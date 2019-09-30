2155a665db84f940d1a317c382866e7c2894021d
miftsmkbatikpwr <miftsmkbatikpwr@gmail.com>
2019-09-30 08:23:46 +0700

    Membuat Simple MVC

---
+++ model.php
@@ -0,0 +1,12 @@
+<?php
+	class Model {
+		public function show_message() {
+			return array(
+				'title' => "MVC",
+				'header' => "APLIKASI WEB SEDERHANA",
+				'konten' => "Halo ini isi halaman web dengan MVC",
+				'footer' => "@Copyright SMK BATIK 2019"
+			);
+		}
+	}
+?>
---
+++ view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?=$data['title']?></title>
+</head>
+<body>
+	<h1><?=$data['header']?></h1>
+	<hr>
+	<h3><?=$data['konten']?></h3>
+	<hr>
+	<?=$data['footer']?>
+</body>
+</html>
